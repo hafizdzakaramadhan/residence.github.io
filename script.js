3ae0e70ec6469739d252899a74b2d9422e712597
hafizdzakaramadhan <hafizdzakaramadhan@gmail.com>
2025-06-25 17:39:17 +0700

    create

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar-menu");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("open");
+};
+
+//menu ditutup saat klik di luar menu
+document.addEventListener("click", (event) => {
+  if (!menu.contains(event.target) && !navbar.contains(event.target)) {
+    navbar.classList.remove("open");
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("open");
+    menu.classList.remove("bx-x");
+  }
+});
+
+//AOS & GSAP
+
+AOS.init();
